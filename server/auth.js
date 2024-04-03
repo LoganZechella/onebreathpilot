4799dd4a6e29622af9e4cbb872f2fb2f2dbd0308
Logan Zechella <jloganzechella@gmail.com>
2024-04-02 21:24:54 -0400

    Fully functioning auth with Firebase securely on backend with email/password and Google

--- server/auth.js
+++ server/auth.js
@@ -21,6 +21,10 @@ const auth = getAuth(app);
 
 // Listen for the document to be loaded
 document.addEventListener('DOMContentLoaded', () => {
+    if (!auth.currentUser) {
+        document.querySelector('.blocker').style.display = 'none';
+        document.getElementById('sign-in-container').style.display = 'block';
+    }
     document.getElementById('show-sign-in').addEventListener('click', (event) => {
         event.preventDefault(); // Prevent the link from following the URL
         if (document.getElementById('show-sign-in').classList.contains('logged-in')) {
@@ -49,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(response => response.json())
             .then(data => {
-                console.log('Login Successful:', data);
+                document.querySelector('.blocker').style.display = 'flex'; // Hide the blocker
                 document.getElementById('sign-in-container').style.display = 'none'; // Hide the sign-in form
                 document.getElementById('show-sign-in').classList.add('logged-in'); // Update the UI to show the user is logged in
                 document.getElementById('show-sign-in').textContent = 'Sign Out'; // Change the text to "Sign Out"
@@ -91,9 +95,12 @@ document.addEventListener('DOMContentLoaded', () => {
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log('User is signed in:', user);
+        document.querySelector('.blocker').style.display = 'flex'; // Hide the blocker
+        document.getElementById('sign-in-container').style.display = 'none'; // Hide the sign-in form
         document.getElementById('show-sign-in').classList.add('logged-in'); // Update the UI to show the user is logged in
         document.getElementById('show-sign-in').textContent = 'Sign Out'; // Change the text to "Sign Out"
     } else {
+        document.querySelector('.blocker').style.display = 'none'; // Show the blocker
         document.getElementById('show-sign-in').classList.remove('logged-in'); // Update the UI to show the user is logged out
         document.getElementById('show-sign-in').textContent = 'Sign In'; // Change the text to "Sign In"
     }
